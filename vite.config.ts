f6f76d4f7588b59b5e018108ac8de60f21ebcdf5
ykx2Lon <ykx2Lon@gmail.com>
2026-02-23 12:22:44 +0800

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue_color-change-button/',
+  base: '/vue_QMK_keyboard/',
 })
